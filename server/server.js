751bfec7985f3759ecb0f0bb9b9f5455eb058322
Angel Lopez <UTM20040095@utma.edu.mx>
2022-06-23 11:51:51 -0500

    Cambios 23/06/2022

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const routes = require('./routes/index');
 var body_parser = require('body-parser');
 app.use(body_parser.urlencoded({extended:true}));
 
-app.use("/api",routes);
+app.use("/api", routes);
 
 app.listen(process.env.PORT, () => {
     console.log("ONLINE".green  + "Se esta escuchando en el puerto 3000")
